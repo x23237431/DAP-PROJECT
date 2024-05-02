95143f491e86e50af08d18df12266efc24fb6b04
sangeetha230 <x23219602@student.ncirl.ie>
2024-05-02 01:11:00 +0100

    pipeline

--- pipeline.py
+++ pipeline.py
@@ -11,6 +11,7 @@ import psycopg2
     ins={"dataset1": In(),"dataset3": In()},
     out={
         "all_violations_df": Out(dagster_type=pd.DataFrame),
+        "all_inspections_df": Out(dagster_type=pd.DataFrame),
         "all_cases_df": Out(dagster_type=pd.DataFrame)
     }
 )
@@ -22,11 +23,13 @@ def getting_mongo_data(context: OpExecutionContext, dataset3 ):
 
         # Retrieve and convert data from "enforcement" collection
         all_violations = list(db["all_violations"].find())
+        all_inspections = list(db["all_inspections"].find())
         all_cases = list(db["all_cases"].find())
         context.log.info("Fetched and converted enforcement data.")
 
         # Retrieve and convert data from "occupancy" collection
         all_violations_df = pd.DataFrame(all_violations)
+        all_inspections_df = pd.DataFrame(all_inspections)
         all_cases_df = pd.DataFrame(all_cases)
         context.log.info("Fetched and converted occupancy data.")
 
@@ -44,6 +47,7 @@ def getting_mongo_data(context: OpExecutionContext, dataset3 ):
 @op(
     ins={
         "all_violations_df": In(dagster_type=pd.DataFrame),
+        "all_inspections_df": In(dagster_type=pd.DataFrame),
         "all_cases_df": In(dagster_type=pd.DataFrame)
     },
     out=Out(dagster_type=pd.DataFrame)
@@ -54,12 +58,14 @@ def transform(context: OpExecutionContext,all_violations_df: pd.DataFrame, all_c
     try:
         # Drop unnecessary columns
         all_violations_df.drop(columns=['_id', 'LastUpload'], inplace=True)
+        all_inspections_df.drop(columns=['_id', 'row_id', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7'], inplace=True) # type: ignore
         all_cases_df.drop(columns=['_id', 'row_id', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col8', 'col7', 'NextHearingDate'], inplace=True)
         context.log.debug("Dropped unnecessary columns.")
 
         # Convert columns to integer
         int_columns = {
             'all_violations_df': ['CaseID', 'ViolationID'],
+            'all_inspections_df': ['CaseID', 'Check'],
             'all_cases_df': ['OBJECTID', 'Zipcode']
         }
         for df_name, cols in int_columns.items():
@@ -69,16 +75,21 @@ def transform(context: OpExecutionContext,all_violations_df: pd.DataFrame, all_c
         
          # Handle string extraction
         all_violations_df['place'] = all_violations_df['Location'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', str(x)).strip())
+        all_inspections_df['place'] = all_inspections_df['location'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', str(x)).strip())
+        context.log.debug("Extracted and cleaned location data.")
 
         # Convert datetime columns
         all_cases_df['STATUS DATE'] = pd.to_datetime(all_cases_df['Status Date'], format='%Y%m%d%H%M%S.%f').dt.date
         context.log.info("Converted datetime columns.")
         all_violations_df['VIOLATIONDATE'] = pd.to_datetime(all_violations_df['ViolationDate'], format='%m/%d/%Y %I:%M:%S %p', errors='coerce').dt.date
         context.log.info("Converted datetime columns.")
+        all_inspections_df['INSPECTION DATE'] = pd.to_datetime(all_inspections_df['InspectionDate'], errors='coerce').dt.date
+        context.log.info("Converted datetime columns.")
 
+        
         # Rename overlapping columns before merging
-        dfs = [all_cases_df]
-        df_names = ['all_violations_df','all_cases_df']
+        dfs = [all_violations_df, all_cases_df, all_inspections_df]
+        df_names = ['all_violations_df','all_cases_df','all_inspections_df']
         column_counts = {}
         for df, df_name in zip(dfs, df_names):
             for column in df.columns:
@@ -92,7 +103,7 @@ def transform(context: OpExecutionContext,all_violations_df: pd.DataFrame, all_c
                     df.rename(columns={column: df_name + '_' + column}, inplace=True)
         
         # Concatenate dataframes
-        mixed_df = pd.concat([all_violations_df, all_cases_df], ignore_index=True)
+        mixed_df = pd.concat([all_violations_df, all_inspections_df, all_cases_df], ignore_index=True)
         context.log.info("Combined dataframes into a single DataFrame.")
         return mixed_df
         #return Output(mixed_df, "mixed_df")
@@ -102,6 +113,23 @@ def transform(context: OpExecutionContext,all_violations_df: pd.DataFrame, all_c
         context.log.error(f"An error occurred during transformation and load: {e}")
         raise
 
-        
+@op(
+    ins={
+        "mixed_df": In(dagster_type=pd.DataFrame)
+    }
+)
+def load(context: OpExecutionContext, mixed_df: pd.DataFrame):
+    context.log.info("Preparing to push data to the PostgreSQL database.")
+
+    # Construct connection string
+    connection_string = 'postgresql://postgres:groupg@localhost:5432/group_g'
+    
+    # Create SQLAlchemy engine
+    engine = create_engine(connection_string)
+    
+    # Push DataFrame to PostgreSQL
+    mixed_df.to_sql("enforcement_table", engine, if_exists='replace', index=False)
+    
+    context.log.info("Data has been successfully pushed to PostgreSQL database.")       
 
 
