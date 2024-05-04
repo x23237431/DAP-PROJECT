1485235acb8d4783db214d032ab978be5fd21b9a
sangeetha230 <157593368+sangeetha230@users.noreply.github.com>
2024-05-04 12:28:07 +0100

    pipeline.py Final Code

--- pipeline.py
+++ pipeline.py
@@ -7,15 +7,16 @@ from data_ingestion import *
 import re
 import psycopg2
 
+#op
 @op(
-    ins={"dataset1": In(),"dataset3": In()},
+    ins={"dataset1": In(), "dataset2": In()},
     out={
         "all_violations_df": Out(dagster_type=pd.DataFrame),
         "all_inspections_df": Out(dagster_type=pd.DataFrame),
         "all_cases_df": Out(dagster_type=pd.DataFrame)
     }
 )
-def getting_mongo_data(context: OpExecutionContext, dataset3 ):
+def getting_mongo_data(context: OpExecutionContext,  dataset1, dataset2 , dataset3 ):
     try:
         # Connect to MongoDB
         client = pymongo.MongoClient("mongodb://dapsem1:dap_sem1@localhost:27017/admin")
@@ -34,7 +35,7 @@ def getting_mongo_data(context: OpExecutionContext, dataset3 ):
         context.log.info("Fetched and converted occupancy data.")
 
         # Output the dataframes
-        return Output(all_violations_df, "all_violations_df"),Output(all_cases_df, "all_cases_df")
+        return Output(all_violations_df, "all_violations_df"), Output(all_inspections_df, "all_inspections_df"), Output(all_cases_df, "all_cases_df")
 
     except pymongo.errors.ConnectionError as e:
         context.log.error(f"Failed to connect to MongoDB: {e}")
@@ -52,66 +53,124 @@ def getting_mongo_data(context: OpExecutionContext, dataset3 ):
     },
     out=Out(dagster_type=pd.DataFrame)
 )
-def transform(context: OpExecutionContext,all_violations_df: pd.DataFrame, all_cases_df: pd.DataFrame) -> pd.DataFrame:
+def transform(context: OpExecutionContext, all_violations_df: pd.DataFrame, all_inspections_df: pd.DataFrame, all_cases_df: pd.DataFrame) -> pd.DataFrame:
     context.log.info("Starting the data transformation process.")
     
     try:
         # Drop unnecessary columns
         all_violations_df.drop(columns=['_id', 'LastUpload'], inplace=True)
-        all_inspections_df.drop(columns=['_id', 'row_id', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7'], inplace=True) # type: ignore
+        all_inspections_df.drop(columns=['_id', 'row_id', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col7'], inplace=True)
         all_cases_df.drop(columns=['_id', 'row_id', 'col1', 'col2', 'col3', 'col4', 'col5', 'col6', 'col8', 'col7', 'NextHearingDate'], inplace=True)
         context.log.debug("Dropped unnecessary columns.")
 
         # Convert columns to integer
-        int_columns = {
-            'all_violations_df': ['CaseID', 'ViolationID'],
-            'all_inspections_df': ['CaseID', 'Check'],
-            'all_cases_df': ['OBJECTID', 'Zipcode']
-        }
-        for df_name, cols in int_columns.items():
-            for col in cols:
-                locals()[df_name][col] = locals()[df_name][col].fillna(0).astype(int)
-        context.log.info("Converted specified columns to integer.")
-        
-         # Handle string extraction
+
+        int_violations = ['CaseID','ViolationID']
+
+        int_inspections = ['CaseID','Check']
+
+        int_cases =  ['OBJECTID','Zipcode']
+
+        for col in int_violations:
+            all_violations_df[col] = all_violations_df[col].fillna(0).astype(int)
+
+        for col in int_inspections:
+            all_inspections_df[col] = all_inspections_df[col].fillna(0).astype(int)
+            
+        for col in int_cases:
+            all_cases_df[col] = all_cases_df[col].fillna(0).astype(int) 
+
+        #using regex created a new column name Place in all the datasets
         all_violations_df['place'] = all_violations_df['Location'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', str(x)).strip())
         all_inspections_df['place'] = all_inspections_df['location'].apply(lambda x: re.sub(r'[^a-zA-Z\s]', '', str(x)).strip())
-        context.log.debug("Extracted and cleaned location data.")
+        all_cases_df['Status Date'] = pd.to_datetime(all_cases_df['Status Date'], format='%Y%m%d%H%M%S.%f')
+        all_cases_df['Status Date'] = all_cases_df['Status Date'].dt.date
+        all_inspections_df['InspectionDate'] = pd.to_datetime(all_inspections_df['InspectionDate'], errors='coerce')
+        all_inspections_df['InspectionDate'] = all_inspections_df['InspectionDate'].dt.date
+        all_violations_df['ViolationDate'] = pd.to_datetime(all_violations_df['ViolationDate'], format='%m/%d/%Y %I:%M:%S %p')
+        all_violations_df['ViolationDate'] = all_violations_df['ViolationDate'].dt.date
 
-        # Convert datetime columns
-        all_cases_df['STATUS DATE'] = pd.to_datetime(all_cases_df['Status Date'], format='%Y%m%d%H%M%S.%f').dt.date
-        context.log.info("Converted datetime columns.")
-        all_violations_df['VIOLATIONDATE'] = pd.to_datetime(all_violations_df['ViolationDate'], format='%m/%d/%Y %I:%M:%S %p', errors='coerce').dt.date
-        context.log.info("Converted datetime columns.")
-        all_inspections_df['INSPECTION DATE'] = pd.to_datetime(all_inspections_df['InspectionDate'], errors='coerce').dt.date
-        context.log.info("Converted datetime columns.")
 
-        
-        # Rename overlapping columns before merging
+        #Defined the datatypes of each column
+        all_violations_df = all_violations_df.astype({
+            'CaseID': int, 
+            'ViolationID' : int,
+            'CaseNo' :  'string',
+            'Location' : 'string',
+            'ViolationDate' : 'datetime64[ns]',
+            'CodeSection' : 'string',
+            'Violation' : 'string',
+            'Description' : 'string',
+            'place' : 'string',
+        })
+        all_inspections_df=all_inspections_df.astype({
+            'CaseID': int, 
+            'CaseNo' : 'string',
+            'location' : 'string',
+            'InspectionDate' : 'datetime64[ns]',
+            'InspectionType' : 'string',
+            'InspectionResult' : 'string',
+            'Inspector' : 'string',
+            'place' : 'string',
+            'Check' : int,
+        })
+        all_cases_df=all_cases_df.astype({
+            'OBJECTID': int, 
+            'CaseNo' : 'string',
+            'GeoPIN' : 'string',
+            'Status Date' : 'datetime64[ns]',
+            'Open/Closed' : 'string',
+            'Permit Status' : 'string',
+            'Previous Hearing Result' : 'string',
+        'Stage':'string',
+            'Zipcode' : int,
+        })
+    
+    
+        # List to hold all dataframes
         dfs = [all_violations_df, all_cases_df, all_inspections_df]
-        df_names = ['all_violations_df','all_cases_df','all_inspections_df']
+
+        # List to hold the names of dataframes
+        df_names = ['all_violations_df', 'all_cases_df', 'all_inspections_df']
+
+        # Dictionary to keep track of column names and their counts
         column_counts = {}
+
+        # Iterate over each DataFrame
         for df, df_name in zip(dfs, df_names):
+            # Iterate over each column in the DataFrame
             for column in df.columns:
+                # If column name exists in the dictionary, increment its count
                 if column in column_counts:
                     column_counts[column] += 1
+                # Otherwise, initialize its count to 1
                 else:
                     column_counts[column] = 1
+
+        # Iterate over each DataFrame again to rename columns
         for df, df_name in zip(dfs, df_names):
+            # Iterate over each column in the DataFrame
             for column in df.columns:
+                # If the column exists in more than one DataFrame, rename it
                 if column_counts[column] > 1:
                     df.rename(columns={column: df_name + '_' + column}, inplace=True)
-        
-        # Concatenate dataframes
+        all_violations_df = all_violations_df.rename(str.upper, axis='columns')
+        all_cases_df = all_cases_df.rename(str.upper, axis='columns')
+        all_inspections_df = all_inspections_df.rename(str.upper, axis='columns')
+
+
+        context.log.debug("Extracted and cleaned location data.")
+
+        #merged all the three datasets 
         mixed_df = pd.concat([all_violations_df, all_inspections_df, all_cases_df], ignore_index=True)
+        
         context.log.info("Combined dataframes into a single DataFrame.")
         return mixed_df
         #return Output(mixed_df, "mixed_df")
-
-        
     except Exception as e:
         context.log.error(f"An error occurred during transformation and load: {e}")
         raise
+
 
 @op(
     ins={
@@ -130,6 +189,4 @@ def load(context: OpExecutionContext, mixed_df: pd.DataFrame):
     # Push DataFrame to PostgreSQL
     mixed_df.to_sql("enforcement_table", engine, if_exists='replace', index=False)
     
-    context.log.info("Data has been successfully pushed to PostgreSQL database.")       
-
-
+    context.log.info("Data has been successfully pushed to PostgreSQL database.")
